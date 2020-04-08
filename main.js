bfcd2ae3af1c180c354f8ea5da51b468f05e17c5
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-04-08 12:23:04 +0100

    js: drop G analytics

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Google Analytics!
-////////////////////////////////////////////////////////////////////////////////
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// see below for tracking calls
-
-////////////////////////////////////////////////////////////////////////////////
 // Utility methods
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -458,8 +448,4 @@ $(function(){
   } else {
     console.log( "No refresh time set, not refreshing. Use URL param refreshAfterSeconds to refresh after a period of inactivity." );
   }
-
-  var uid = getUrlParameter( "uid" );
-  ga('create', 'UA-40440647-2', { 'userId': uid });
-  ga('send', 'pageview');
 });
